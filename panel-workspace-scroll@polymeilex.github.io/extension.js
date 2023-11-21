e1214828aa004fd2cbebb00f848db3d7af10d604
PolyMeilex <marynczakbartlomiej@gmail.com>
2023-11-22 00:07:37 +0100

    Address gnome ext review comments

--- panel-workspace-scroll@polymeilex.github.io/extension.js
+++ panel-workspace-scroll@polymeilex.github.io/extension.js
@@ -1,7 +1,6 @@
-import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-export default class PanelWorkspaceScrollExtension extends Extension {
+export default class PanelWorkspaceScrollExtension {
     enable() {
         this.scrollEventId = Main.panel.connect('scroll-event', (_actor, event) => Main.wm.handleWorkspaceScroll(event));
     }
